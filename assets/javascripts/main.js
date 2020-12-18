c63dee78de7be669bf2d3aec0a0f91e6be6496d5
Toya-Onodera <gtrzexel@gmail.com>
2020-12-18 21:10:10 +0900

    ナンバーを隠すところを実装

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -27,17 +27,86 @@
             image.src = base64Image;
 
             image.onload = () => {
-                const w = image.width;
-                const h = image.height;
+                let w = image.width;
+                let h = image.height;
+                let scale = 1;
+
+                // 画像サイズを小さくする
+                if (w >= (document.documentElement.clientWidth * 0.8)) {
+                    scale = (document.documentElement.clientWidth * 0.8) / w;
+                    w = w * scale;
+                    h = h * scale;
+                }
+
+                if (h >= (document.documentElement.clientHeight * 0.8)) {
+                    scale = (document.documentElement.clientHeight * 0.8) / h;
+                    w = w * scale;
+                    h = h * scale;
+                }
+
                 canvas.width = w;
                 canvas.height = h;
                 ctx.drawImage(image, 0, 0, w, h);
 
                 fileFieldWrapper.style.display = 'none';
-                canvasFieldWrapper.style.display = 'block';
+                canvasFieldWrapper.style.display = 'flex';
 
                 resolve();
             };
         });
+
+        // 画像を GCP 上にアップロードし、ナンバープレートを検出する
+        const GOOGLE_API_KEY = 'YOUR API KEY';
+        const GOOGLE_CLOUD_VISION_API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${GOOGLE_API_KEY}`;
+
+        // GCP にアップロードするには base64 の先頭部分を取り除いた文字列が必要 -> /9j/... の文字列を作成する
+        const sendBase64Image = (file.type === 'image/jpeg') ? base64Image.substr(23) : base64Image.substr(22);
+
+        const requests = {
+            "requests": [
+                {
+                    "image": {
+                        "content": sendBase64Image
+                    },
+                    "features": [
+                        {
+                            "type": "OBJECT_LOCALIZATION"
+                        }
+                    ]
+                }
+            ]
+        };
+
+        const response = await fetch(GOOGLE_CLOUD_VISION_API_URL, {
+            method: 'POST',
+            redirected: true,
+            body: JSON.stringify(requests),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const result = await response.json();
+
+        // localizedObjectAnnotations 内に推定結果が格納されている
+        if ('localizedObjectAnnotations' in result['responses'][0]) {
+            // ナンバーの推定結果が存在しているか調べる
+            const localizedObjectAnnotations = result['responses'][0]['localizedObjectAnnotations'];
+            const licensePlateIndex = localizedObjectAnnotations.findIndex((e) => e.name === 'License plate');
+
+            // -1 が返ってきたらナンバーが推定されなかった
+            if (licensePlateIndex !== -1) {
+                // バウンディングボックス は左上から時計回りに返ってくる
+                // 正則化された値が返ってくるので実際の座標データに変換する
+                // とりあえず、左上が分かれば大丈夫なのでその部分だけ取り出す
+                const plateStartX = Math.round(canvas.width * localizedObjectAnnotations[licensePlateIndex]['boundingPoly']['normalizedVertices'][0]['x']);
+                const plateStartY = Math.round(canvas.height * localizedObjectAnnotations[licensePlateIndex]['boundingPoly']['normalizedVertices'][0]['y'])
+                const plateWidth = Math.round(canvas.width * localizedObjectAnnotations[licensePlateIndex]['boundingPoly']['normalizedVertices'][1]['x']) - Math.round(canvas.width * localizedObjectAnnotations[licensePlateIndex]['boundingPoly']['normalizedVertices'][0]['x']);
+                const plateHeight = Math.round(canvas.height * localizedObjectAnnotations[licensePlateIndex]['boundingPoly']['normalizedVertices'][3]['y']) - Math.round(canvas.height * localizedObjectAnnotations[licensePlateIndex]['boundingPoly']['normalizedVertices'][0]['y']);
+
+                ctx.fillStyle = 'rgb(255, 255, 255)';
+                ctx.fillRect(plateStartX, plateStartY, plateWidth, plateHeight);
+            }
+        }
     });
 })();
